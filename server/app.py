e06de1a0d8d64940d9acad6a6cfb4bab72daffc4
AnitaNamjoon <anitachepkemoi1@gmail.com>
2023-09-26 02:53:51 +0300

    New commit

--- server/app.py
+++ server/app.py
@@ -9,19 +9,17 @@ from models import db, Plant
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+app.json_encoder.compact = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
 
-
 class Plants(Resource):
-
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
+        return jsonify(plants), 200
 
     def post(self):
         data = request.get_json()
@@ -35,21 +33,43 @@ class Plants(Resource):
         db.session.add(new_plant)
         db.session.commit()
 
-        return make_response(new_plant.to_dict(), 201)
+        return jsonify(new_plant.to_dict()), 201
 
+  
+    def patch(self, id):
+        data = request.get_json()
+        plant = Plant.query.get(id)
+
+        if not plant:
+            return make_response(jsonify({'error': 'Plant not found'}), 404)
+
+        
+        if 'is_in_stock' in data:
+            plant.is_in_stock = data['is_in_stock']
+
+        db.session.commit()
+        return jsonify(plant.to_dict()), 200
 
 api.add_resource(Plants, '/plants')
 
-
 class PlantByID(Resource):
-
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.get(id)
+        if not plant:
+            return make_response(jsonify({'error': 'Plant not found'}), 404)
+        return jsonify(plant.to_dict()), 200
 
+    
+    def delete(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return make_response('', 204)
+
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('', 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
